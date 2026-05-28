e208f36a4206d0928f1bb442190bc5b5252845fd
appropose023-tech <appropose023@gmail.com>
2026-05-28 16:56:21 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -72,12 +72,7 @@ class _LoginPageState extends State<LoginPage> {
            "Content-Type": "application/json",
       },
      ),
-
-      options: Options(
-
-        contentType:
-            Headers.formUrlEncodedContentType,
-      ),
+      
     );
 
     print(
